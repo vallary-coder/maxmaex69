a4ee8755e8c3de1f9300aa77b35905094682e38f
vallary-coder <chepkoechvallary123@gmail.com>
2024-10-15 09:37:02 -0700

    Add files via upload

---
+++ val chepkoech.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c;
+
+    printf("Enter the first value: ");
+    scanf("%d", &a);
+
+    printf("Enter the second value: ");
+    scanf("%d", &b);
+
+    c = a + b;
+
+    printf("%d + %d = %d\n", a, b, c);
+
+    return 0;
+}
